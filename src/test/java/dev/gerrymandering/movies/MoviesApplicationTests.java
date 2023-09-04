3a8f847bf9445a02ba99da196b93319d61e2f1dd
gerry-mandering <kimmen1119@gmail.com>
2023-09-04 20:02:31 +0900

    Feat: movie module

---
+++ src/test/java/dev/gerrymandering/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gerrymandering.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
